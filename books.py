4e99b3b42a1b6c5c84ec497d78205ff508a70c04
Nanaaaaaaa19 <art.nblue.pro@gmail.com>
2024-10-16 06:15:08 +0900

    db connected to API

--- books.py
+++ books.py
@@ -6,7 +6,6 @@
 
 from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel, Field
-from uuid import UUID
 import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -51,7 +50,7 @@ def create_book(book: Book, db: Session = Depends(get_db)):
     book_model.description = book.description
     book_model.rating = book.rating
 
-    db.add(book_model())
+    db.add(book_model)
     db.commit() #commit the model to db
 
     return book
@@ -59,29 +58,39 @@ def create_book(book: Book, db: Session = Depends(get_db)):
 
 #implement http exeception to catch errors that pydantic won't catch like business errors 
 @app.put("/{book_id}") #put in parameter the book id 
-def update_book(book_id: UUID, book: Book):
-    counter = 0
-    #go thru the list if the same id then we gonna update the book informations and swap the data in the api
-    for x in BOOKS:
-        if x.id == book_id:
-            BOOKS[counter - 1] = book #bc count list starts at 0 1st case
-            return f"{BOOKS[counter - 1]} has been updated" #Response message on the API display  
-    raise HTTPException(
-        status_code=404,
-        detail= f"ID {book_id} does not exist"    
-    )
+def update_book(book_id: int, book: Book, db: Session =Depends(get_db)):
+
+    book_model = db.query(models.Books).filter(models.Books.id == book_id).first()
+    #meaning looks thru every record until models.Books.id == book_id we are passing in
+    #.first() = when found it thezn return it 
+
+    if book_model is None:
+        raise HTTPException(
+            status_code=404,
+            detail= f"ID {book_id} does not exist"    
+        )
+    
+    book_model.title = book.title
+    book_model.author = book.author
+    book_model.description = book.description
+    book_model.rating = book.rating
+
+    db.add(book_model)
+    db.commit()
+
+    return book
 
             #this is called the path parameter
 @app.delete("/{book_id}")
-def delete_book(book_id: UUID):
-    counter = 0
-    for x in BOOKS:
-        counter += 1
-        if x.id == book_id:
-            del BOOKS[counter - 1]
-            return f"ID {book_id} is deleted."
-    raise HTTPException(
-        status_code=404, 
-        detail= f"ID {book_id} does not exist"
-    )
+def delete_book(book_id: int, db: Session = Depends(get_db)):
 
+    book_model = db.query(models.Books).filter(models.Books.id == book_id).first() #make sur the db exists
+
+    if book_model is None:
+        raise HTTPException(
+            status_code=404, 
+            detail= f"ID {book_id} does not exist"
+        )
+    
+    db.query(models.Books).filter(models.Books.id == book_id).delete()
+    db.commit()
